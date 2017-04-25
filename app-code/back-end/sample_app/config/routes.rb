b448095611015132246a962102f9acb691b29511
Carlos Adolfo Ortiz Quiros (Mac) <carlosaortiz@yahoo.com>
2017-04-25 06:37:33 -0500

    Updates user controller assets.

--- app-code/back-end/sample_app/config/routes.rb
+++ app-code/back-end/sample_app/config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
+  # Root
   root 'static_pages#home'
-  get  '/help',    to: 'static_pages#help'
-  get  '/about',   to: 'static_pages#about'
-  get  '/contact', to: 'static_pages#contact'
+
+  # Dispatched by StaticPagesController
+  get '/help',    to: 'static_pages#help'
+  get '/about',   to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+
+  # Dispatched by UsersController
+  get '/signup', to: 'users#new'
 end
--- app-code/back-end/sample_app/test/controllers/users_controller_test.rb
+++ app-code/back-end/sample_app/test/controllers/users_controller_test.rb
@@ -2,8 +2,13 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get users_new_url
+    get signup_path
     assert_response :success
   end
 
+  test "should get sign up" do
+    get signup_path
+    assert_response :success
+    assert_select "title", full_title("Sign up")
+  end
 end
