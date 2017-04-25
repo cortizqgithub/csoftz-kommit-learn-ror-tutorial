fc688354e202db640cd8501e0f9bd9f99240d991
Carlos Adolfo Ortiz Quiros (Mac) <carlosaortiz@yahoo.com>
2017-04-25 05:42:53 -0500

    Adds the helper full_title to all tests in application.

--- app-code/back-end/sample_app/test/controllers/static_pages_controller_test.rb
+++ app-code/back-end/sample_app/test/controllers/static_pages_controller_test.rb
@@ -1,8 +1,9 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  # Initializes test fixture.
   def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
+    @base_title = "R1uby on Rails Tutorial Sample App"
   end
 
   test "should get root" do
@@ -13,19 +14,20 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get help" do
     get help_path
     assert_response :success
-    assert_select "title", "Help | #{@base_title}"
+    assert_select "title", full_title("Help")
   end
   
   test "should get about" do
     get about_path
     assert_response :success
-    assert_select "title", "About | #{@base_title}"
+    assert_select "title", full_title("About")
   end
 
   test "should get contact" do
     get contact_path
     assert_response :success
-    assert_select "title", "Contact | #{@base_title}"
+    #assert_select "title", "Contact | #{@base_title}"
+    assert_select "title", full_title("Contact")
   end
 
 end
