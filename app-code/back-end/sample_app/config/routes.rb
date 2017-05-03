c226d4a53dbdc52b281a75da0cd30af8f0b42eb3
Carlos Adolfo Ortiz Quiros (Mac) <carlosaortiz@yahoo.com>
2017-05-03 14:17:03 -0500

    Adjusts signup URLs and commits integration tests.

--- app-code/back-end/sample_app/config/routes.rb
+++ app-code/back-end/sample_app/config/routes.rb
@@ -3,13 +3,14 @@ Rails.application.routes.draw do
   root 'static_pages#home'
 
   # Dispatched by StaticPagesController
-  get '/help',    to: 'static_pages#help'
-  get '/about',   to: 'static_pages#about'
-  get '/contact', to: 'static_pages#contact'
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
 
   # Dispatched by UsersController
-  get '/signup', to: 'users#new'
+  get  '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
 
-  # Users routes
+  # Users routes (REST API for Users)
   resources :users
 end
