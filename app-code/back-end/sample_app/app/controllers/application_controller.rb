a4733b49913fd397db3d14fc3a11cf0bbb079e2a
Carlos Adolfo Ortiz Quiros (Mac) <carlosaortiz@yahoo.com>
2017-05-08 08:55:36 -0500

    Adds user helper and valid/invalid user login validation.

--- app-code/back-end/sample_app/app/controllers/application_controller.rb
+++ app-code/back-end/sample_app/app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # By including this helper here, it is available in any controller 
+  include SessionsHelper
 end
--- app-code/back-end/sample_app/app/controllers/sessions_controller.rb
+++ app-code/back-end/sample_app/app/controllers/sessions_controller.rb
@@ -6,11 +6,18 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
+      log_in user
+      redirect_to user # can also be written as redirect_to user_url(user)
     else
       # Create an error message.
       flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
       render 'new'
     end
+  end
+
+  # Returns the current logged-in user (if any).
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
   end
 
   def destroy
