3266d904f51d126325d4e2b4ec7014bec125e939
Carlos Adolfo Ortiz Quiros (Mac) <carlosaortiz@yahoo.com>
2017-05-22 11:14:29 -0500

    Commits the succesful user update (edit).

--- app-code/back-end/sample_app/app/models/user.rb
+++ app-code/back-end/sample_app/app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                                     format: { with: VALID_EMAIL_REGEX },
                                     uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   has_secure_password
 
   # For a discussion of self.new_token and self.remember_token or the other
